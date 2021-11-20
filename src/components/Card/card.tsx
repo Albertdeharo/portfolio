51eb0a7cd35541a293429bde549c31ddaba427cf
Albert de Haro <Albert.deharo.murcia@gmail.com>
2021-11-20 13:39:00 +0100

    bug

--- src/components/Card/card.tsx
+++ src/components/Card/card.tsx
@@ -2,7 +2,7 @@ import './../Card/card.scss';
 
 const Card = () => (
   <div className="card">
-    <div class="glass-panel">
+    <div className="glass-panel">
       <h1><a href="https://superdevresources.com/glassmorphism-ui-css/" target="_blank">Glassmorphism CSS Panels and Buttons</a></h1>
       <p>Glassmorphism is achieved using transparency and background blur to get a frosted-glass like effect.</p>
       <div className="neon-button">
