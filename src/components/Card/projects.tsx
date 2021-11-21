28ca86ebc786aa0b59f378f55e98dcf19ac0d6b5
Albert de Haro <Albert.deharo.murcia@gmail.com>
2021-11-21 19:25:19 +0100

    linked explore landing project

--- src/components/Card/projects.tsx
+++ src/components/Card/projects.tsx
@@ -6,7 +6,7 @@ const Projects = () => (
       <h1>PROJECTS</h1>
       <p>PROJECTS</p>
       <div className="neon-button">
-        <a className="glass-button" href="https://google.com" rel="noreferrer">ACTION 1</a>
+        <a className="glass-button" href="https://albertdeharo.github.io/explore-landing/" target="_blank" rel="noreferrer">Explore landing</a>
         <a className="glass-button" href="https://google.com" rel="noreferrer">ACTION 2</a>
       </div>
     </div>
